e3ec71d5c6335afe02d6ab7a87e936e2a158bf1c
Kramer <SE-kramerje@oregonstate.edu>
2020-01-29 10:28:40 -0800

    Added way to handle xlsx and xls files (convert to csv) and added a potential source

--- csv_transformer.py
+++ csv_transformer.py
@@ -2,7 +2,77 @@ import os
 import csv # used to read and write csv files
 import string
 
+# function that checks to see if the file passed in is of type .xls/.xlsx
+def is_xls_xlsx(file):
+    good_file = [".xls", ".xlsx"]
+    #get index of last period
+    index = file.rfind('.')
+    ext = file[index:]
+    # handle extensions in all caps
+    ext = ext.lower()
+     # if the file extension is something other than xls/xlsx, return false
+    if ext not in good_file:
+        return False
+    # otherwise, return true! file is xls/xlsx
+    return True
+
+# accepts xls and xlsx files and their paths and converts to csv file
+def convert_xls_xlsx_to_csv(filename, filepath):
+    # 'pip install xlrd' in command line
+    import xlrd
+    # list of good sheet names to return
+    good_sheets = []
+    # list of sheets that we don't want to convert
+    bad_sheets = ['T&C', 'XDO_METADATA', "Summary"];
+    # error handling for file opening
+    try:
+        # open file
+        wb = xlrd.open_workbook(filepath)
+    except FileNotFoundError:
+        print("XLS/XLSX File Not Found")
+        return "error"
+    # get all psible sheet names (could be more than 1)
+    sheets = wb.sheet_names()
+
+    for sheet in sheets:
+    # check if the sheet is a bad sheet
+        if sheet in bad_sheets:
+        #     # skip the sheet if it is
+            continue
+        else:
+            break
+    # get specific sheet as a Sheet object (needed for sh.nrows below)
+    sh = wb.sheet_by_name(sheet)
+    # head is raw file name without its file extension
+    head, sep, tail = filename.partition('.')
+    # append csv file extension to string
+    # head += sheet + ".csv" ******** ONLY WITH MULTIPLE SHEETS
+    head += ".csv"
+    # create csv file with same name
+    csv_file = open(head, 'w', newline='')
+    wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    # write to csv file
+    for rownum in range(sh.nrows):
+        wr.writerow(sh.row_values(rownum))
+
+    csv_file.close()
+    # add this sheet file path to good sheet list
+    good_sheets.append(csv_file.name)
+
+    # return the array of sheets as csv file (with path) ******ONLY SEND FIRST SHEET SO IT WORKS ON FRONT END || DONT RETURN
+    # return good_sheets[0]
+
 def transform(filename,filepath):
+
+    # first, if the file type is not csv, convert to csv
+    # check to see if the file type is .xls, .xlsx
+    if is_xls_xlsx(filename) == True:
+        # if the file is .xls, .xlsx
+        convert_xls_xlsx_to_csv(filename, filepath)
+        index = filename.rfind('.')
+        ext = filename[index:]
+        filepath = filename.replace(ext, ".csv")
+        filename = filepath
 
     # read csv file
     # opens input file
@@ -44,5 +114,5 @@ def transform(filename,filepath):
             csv_writer.writerow(output_dictionary)
 
 if __name__ == "__main__":
-    # print("test")
-    transform('SCPRC-EST2019-18+POP-RES.csv', 'sources/SCPRC-EST2019-18+POP-RES.csv')
+    # ************* TESTING *************
+    transform('SCPRC-EST2019-18+POP-RES.xlsx', 'sources/SCPRC-EST2019-18+POP-RES.xlsx')
