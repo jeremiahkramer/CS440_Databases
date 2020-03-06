93cc5fd05cb8bb56cd0b16191972812748d60caf
olsencar <olsencar@oregonstate.edu>
2020-03-05 17:19:09 -0800

    Added query mappings

--- website/server.js
+++ website/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
