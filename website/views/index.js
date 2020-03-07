72dad5f5a1bfb622ba6ecb5d9caa7bded9f4a9af
olsencar <olsencar@oregonstate.edu>
2020-03-07 00:24:12 -0800

    Got ChartJS working with our data

--- website/views/index.js
+++ website/views/index.js
@@ -1,128 +1,342 @@
-$(document).ready(function(){//JQuery start
+$(document).ready(function() {//JQuery start
+    const columnsToSelect = {
+        "birth_rate": {
+            "dynamic_labels": false,
+            "labels": ["Birth Rate"],
+            "values": ["birth_rate"]
+        },
+        "crime": {
+            "dynamic_labels": false,
+            "labels": [
+                "Violent",
+                "Murder",
+                "Rape",
+                "Robbery",
+                "Aggravated Assault",
+                "Property",
+                "Burglary",
+                "Larceny",
+                "Motor",
+                "Arson"
+            ],
+            "values": [
+                "violent",
+                "murder",
+                "rape",
+                "robbery",
+                "aggravated_assault",
+                "property",
+                "burglary",
+                "larceny",
+                "motor",
+                "arson"
+            ]
 
-//populate div data
-$(".tablename").click(function() {
-    if($(".left").attr("data-tablename") === ""){ //populate first input
-        $(".left").attr("data-tablename", $(this).text()); //populate its data field with this button's name (table name)
-        $(".lefttext").text($(this).text()); //then update its name as well
-    }
-    else if($(".right").attr("data-tablename") === "") { //populate the second input
-        $(".right").attr("data-tablename", $(this).text()); //populate its data field with this button's name (table name)
-        $(".righttext").text($(this).text()); //then update its name as well
-    }
-});
-
-//clear input upon x click
-$(".x").click(function() {
-    $(this).parent().children(".xytext").text(""); //check sibling with the xytext class (p tag)
-    $(this).parent().attr("data-tablename", "");
-});
-
-$(".graph").click(function() {
-    let l = $(".left").children(".lefttext").text();
-    let r = $(".right").children(".righttext").text();
-    // hardcoded
-    let s = "OR";
-    if(!l || !r){
-        alert("You must select two tables before continuing!");
-        return;
-    }
-    var xhr = new XMLHttpRequest();
-    let data = {
-        left: l,
-        right: r,
-        state: s
+        },
+        "education": {
+            "dynamic_labels": false,
+            "labels": [
+                "< HS Diploma",
+                "HS Diploma Only",
+                "Some College",
+                "Bachelors Degree"
+            ],
+            "values": [
+                "less_than_hs_diploma",
+                "hs_diploma_only",
+                "some_college",
+                "bachelors_degree"
+            ]
+        },
+        "elevation": {
+            "dynamic_labels": false,
+            "labels": [
+                "Highest Elevation",
+                "Lowest Elevation",
+                "AVG Elevation"
+            ],
+            "values": [
+                "highest_elevation",
+                "lowest_elevation",
+                "average_elevation"
+            ]
+        },
+        // "homeless": {
+        //     "dynamic_labels": true,
+        //     "labels": [
+        //         "measures"
+        //     ],
+        //     "values": [
+        //         "num_homeless"
+        //     ]
+        // },
+        "income": {
+            "dynamic_labels": false,
+            "labels": [
+                "AVG Salary"
+            ],
+            "values": [
+                "avg_salary"
+            ]
+        },
+        // "mental_illness": {
+        //     "dynamic_labels": true,
+        //     "labels": [
+        //         "illness"
+        //     ],
+        //     "values": [
+        //         "num_diagnosed"
+        //     ]
+        // },
+        // "mortality": {
+        //     "dynamic_labels": true,
+        //     "labels": [
+        //         "cause_of_death"
+        //     ],
+        //     "values": [
+        //         "num_cases"
+        //     ]
+        // },
+        // "population": {
+        //     "dynamic_labels": true,
+        //     "labels": [
+        //         "age_group"
+        //     ], 
+        //     "values": [
+        //         "total_population"
+        //     ]
+        // },
+        "poverty": {
+            "dynamic_labels": false,
+            "labels": [
+                "Poverty Count"
+            ],
+            "values": [
+                "total_poverty"
+            ]
+        },
+        // "std": {
+        //     "dynamic_labels": true,
+        //     "labels": [
+        //         "std"
+        //     ],
+        //     "values": [
+        //         "num_cases"
+        //     ]
+        // },
+        "unemployment": {
+            "dynamic_labels": false,
+            "labels": [
+                "Unemployment Rate"
+            ],
+            "values": [
+                "unemployment_rate"
+            ]
+        },
+        // "voting": {
+        //     "dynamic_labels": true,
+        //     "labels": [
+        //         "party"
+        //     ],
+        //     "values": [
+        //         "party_count"
+        //     ]
+        // },
+        "weather": {
+            "dynamic_labels": false,
+            "labels": [
+                "AVG Temp"
+            ],
+            "values": [
+                "avg_temp"
+            ]
+        }
     };
-    var jsonData = JSON.stringify(data);
-    console.log(jsonData);
-    xhr.open("post", "/", true);
-    xhr.setRequestHeader('Content-type','application/json');
-    xhr.send(jsonData);
-    //recieve the data
-    xhr.onload = function(){
-        let res = JSON.parse(xhr.response);
-        console.log(res);
-      };
-});
 
-$('#select-states').click((e) => {
-    let selected = $('#select-states').val();
+    //populate div data
+    $(".tablename").click(function() {
+        if($(".left").attr("data-tablename") === ""){ //populate first input
+            $(".left").attr("data-tablename", $(this).text()); //populate its data field with this button's name (table name)
+            $(".lefttext").text($(this).text()); //then update its name as well
+        }
+        else if($(".right").attr("data-tablename") === "") { //populate the second input
+            $(".right").attr("data-tablename", $(this).text()); //populate its data field with this button's name (table name)
+            $(".righttext").text($(this).text()); //then update its name as well
+        }
+    });
 
-    // If the user selects all states, then we don't want them to select others as well
-    if ((selected.length > 0 && selected[0] === 'All States') || (e.target.textContent === 'All States')) {
-        $('#select-states').val(['All States']);
+    //clear input upon x click
+    $(".x").click(function() {
+        $(this).parent().children(".xytext").text(""); //check sibling with the xytext class (p tag)
+        $(this).parent().attr("data-tablename", "");
+    });
+
+    $(".graph").click(function() {
+        let l = $(".left").children(".lefttext").text();
+        let r = $(".right").children(".righttext").text();
+        // hardcoded
+        let s = "OR";
+        if(!l || !r){
+            alert("You must select two tables before continuing!");
+            return;
+        }
+        var xhr = new XMLHttpRequest();
+        let data = {
+            left: l,
+            right: r,
+            state: s
+        };
+        var jsonData = JSON.stringify(data);
+        console.log(jsonData);
+        xhr.open("post", "/", true);
+        xhr.setRequestHeader('Content-type','application/json');
+        xhr.send(jsonData);
+        //recieve the data
+        xhr.onload = function(){
+            let res = JSON.parse(xhr.response);
+            console.log(res);
+        };
+    });
+
+    $('#select-states').click((e) => {
+        let selected = $('#select-states').val();
+
+        // If the user selects all states, then we don't want them to select others as well
+        if ((selected.length > 0 && selected[0] === 'All States') || (e.target.textContent === 'All States')) {
+            $('#select-states').val(['All States']);
+        }
+    });
+
+    $('#plot-btn').click(() => {
+        let statesSelected = $('#select-states').val();
+        
+        if (statesSelected.length > 0 && statesSelected[0] === 'All States') {
+            statesSelected = [];
+        }
+
+        const xhr = new XMLHttpRequest();
+        const req = {
+            'states': statesSelected
+        };
+
+        xhr.open('POST', '/query', true);
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.send(JSON.stringify(req));
+
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.response);
+            
+            for (let chart of Object.keys(data)) {
+                if (data.hasOwnProperty(chart)) {
+                    if (columnsToSelect.hasOwnProperty(chart)) {
+                        let datasets = [];
+                        if (columnsToSelect[chart].dynamic_labels) {
+
+                            datasets = data[chart].map(item => {
+                                return {
+                                    label: item[columnsToSelect[chart].labels[0]],
+                                    data: [item[columnsToSelect[chart].values[0]]],
+                                    backgroundColor: randomColor()
+                                }
+                            });
+                        } else {
+                            for (let i = 0; i < columnsToSelect[chart].labels.length; i++) {
+                                datasets.push({
+                                    label: columnsToSelect[chart].labels[i],
+                                    data: data[chart].map(state => state[columnsToSelect[chart].values[i]]),
+                                    backgroundColor: randomColor()
+                                });
+                            }
+                        }
+                        const stacked = datasets.length > 1;
+                        let states = statesSelected;
+                        if (statesSelected.length === 0) {
+                            states = data[chart].map(item => item.state);
+                        }
+                        generateChart(chart + '-chart', chart, datasets, stacked, states);
+                    }
+                }
+            }         
+        }
+    });
+
+    function relClick(){
+        var xhr = new XMLHttpRequest();
+        var newData = data;
+        newData['relevant'] = 1;
+        var jsonData = JSON.stringify(newData);
+        xhr.open("POST","/",true);
+        xhr.setRequestHeader('Content-type','application/json');
+        xhr.send(jsonData);
+        console.log("Clicked Relevant");
+
+
+        xhr = new XMLHttpRequest();
+        xhr.open("GET","/newtweet",true);
+        xhr.send();
+        xhr.onload = function(){
+            var check = JSON.parse(xhr.response);
+            data = (check['ndata'][0]);
+            newItem(data,check['rc'],check['ic'],check['mc']);
+        };
     }
-});
 
-$('#plot-btn').click(() => {
-    let statesSelected = $('#select-states').val();
-    
-    if (statesSelected.length > 0 && statesSelected[0] === 'All States') {
-        statesSelected = [];
+    function randomColor() {
+        const r = Math.floor(Math.random() * 255);
+        const g = Math.floor(Math.random() * 255);
+        const b = Math.floor(Math.random() * 255);
+        return "rgb(" + r + "," + g + "," + b + ")";    
     }
 
-    const xhr = new XMLHttpRequest();
-    const req = {
-        'states': statesSelected
-    };
-
-    xhr.open('POST', '/query', true);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.send(JSON.stringify(req));
-
-    xhr.onload = () => {
-        console.log(xhr.response);
+    function generateChart(htmlID, chartInternalName, datasets, stacked, states) {
+        const ctx = $(`#${htmlID}`);
+        ctx.show();
+        return new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: states,
+                datasets: datasets
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        stacked: stacked
+                    }],
+                    xAxes: [{
+                        stacked: stacked
+                    }]
+                },
+                responsive: true
+            }
+        });
     }
-    console.log(statesSelected);
-});
+    //Interactive Map Javascript
+    $('#map').usmap({
 
-function relClick(){
-    var xhr = new XMLHttpRequest();
-    var newData = data;
-    newData['relevant'] = 1;
-    var jsonData = JSON.stringify(newData);
-    xhr.open("POST","/",true);
-    xhr.setRequestHeader('Content-type','application/json');
-    xhr.send(jsonData);
-    console.log("Clicked Relevant");
+    //click action
+    click: function(event, data) {
+        $('#clicked-state').text('Selected State: '+data.name);
+        $('#clicked-state').effect('highlight', {color: '#C7F464'}, 2000);
+        getstatedata(data.name);
+    }
 
+    });
 
-    xhr = new XMLHttpRequest();
-    xhr.open("GET","/newtweet",true);
-    xhr.send();
-    xhr.onload = function(){
-        var check = JSON.parse(xhr.response);
-        data = (check['ndata'][0]);
-        newItem(data,check['rc'],check['ic'],check['mc']);
-    };
-}
-//Interactive Map Javascript
-$('#map').usmap({
+    function getstatedata(state_abbrev){
 
-  //click action
-  click: function(event, data) {
-    $('#clicked-state').text('Selected State: '+data.name);
-    $('#clicked-state').effect('highlight', {color: '#C7F464'}, 2000);
-    getstatedata(data.name);
-}
-
-});
-
-function getstatedata(state_abbrev){
-
-  var xhr = new XMLHttpRequest();
-  let data = {
-    state : state_abbrev
-  };
-  var jsonData = JSON.stringify(data);
-  // console.log(jsonData);
-  xhr.open("post", "/stateinfo", true);
-  xhr.setRequestHeader('Content-type','application/json');
-  xhr.send(jsonData);
-  //recieve the data
-  xhr.onload = function(){
-      let res = JSON.parse(xhr.response);
-      console.log(res);
-    };
-}
+        var xhr = new XMLHttpRequest();
+        let data = {
+            state : state_abbrev
+        };
+        var jsonData = JSON.stringify(data);
+        // console.log(jsonData);
+        xhr.open("post", "/stateinfo", true);
+        xhr.setRequestHeader('Content-type','application/json');
+        xhr.send(jsonData);
+        //recieve the data
+        xhr.onload = function(){
+            let res = JSON.parse(xhr.response);
+            console.log(res);
+        };
+    }
 });//JQuery end
